534c67dac0fb3b3f7e4d4c6b30c57ffc917cc5a2
Jack Höhndorf <kyushi@crafterbase.de>
2022-05-27 21:26:15 +0200

    removed -Recurse on line 62, added user output

--- install-champ.ps1
+++ install-champ.ps1
@@ -1,5 +1,5 @@
-############################## SETUP THE SCRIPT ##############################
-echo "Install Champ-Script v.1.0 by Jack Höhndorf | https://github.com/Kyushi-CB"
+############################## SCRIPT SETUP #############################################
+echo "Install Champ Script v.1.0 by Jack Hoehndorf | https://github.com/Kyushi-CB"
 
 sleep 1                            
 
@@ -20,49 +20,63 @@ install-champ.ps1          folder1          folder2          folder3
                              |                |                |
                              |                |                |
                         installer.exe    installer.exe    installer.exe"
+
+
+
 "@
+
 pause
+
 #Set the work directory where the script should be executed.
-$mainDir = "$env:SystemDrive\packages\.!Drivers\"
+$mainDir = "$env:SystemDrive\packages\!Drivers\"
 
 #Set in which order sub-directorys should be accessed.
 $dirOrder = @(
-    "chipset",
-    "audio",
-    "network",
-    "display",
-    "misc",
-    "ds",
-    "bios" 
+    "Chipset",
+    "Audio",
+    "Input",
+    "Memory",
+    "Misc",
+    "Network",
+    "Video",
+    "BIOS" 
 )
 
-#Set Wildcards, which file format should be executed.
+#Set wildcards for which file formats to run.
 $wildcards = 
-    ".exe",
-    ".msi"
+    ".exe"
+    
 
 
 
 ##############################################################################
-                    
 
+#Iterates through ordered directorys defined in "$dirOrder" Array.
 
-#Iterate through ordered dirs defined in "$dirOrder" Array.
 for ($i=0; $i -lt $dirOrder.length; $i++) {
 
-    #set/reset app-index for current iterated folder 
+    #set/reset app-index for current iterated folder. 
     $appIndex=0
 
-    # get all files with extensions defined in "$wildcards" from selected folder, pass it to foreach method
-    Get-ChildItem ($mainDir + $dirOrder[$i] + "\") -Recurse | where {$_.extension -in $wildcards} | 
+    #get all files with extensions defined in "$wildcards" from selected folder, pass it to foreach method.
+    Get-ChildItem ($mainDir + $dirOrder[$i] + "\") | where {$_.extension -in $wildcards} | 
     ForEach-Object {
-        #increase on each iteration
+
+        #increase on each iteration.
         $appIndex++
 
         #pass path + index number from current item as string to clipboard
         Set-Clipboard -Value ($_.DirectoryName + "\" + $appIndex)
  
         #execute selected application, wait until the process is finshed
+        Write-Host $_.FullName "wird jetzt installiert..."
         Start-Process $_.FullName -wait
+
+        #loop starts again after confirming until all items in the current directory are executed
+        Write-Host $_.FullName "wurde installiert."
+        #pause
     }
 }
+Write-Host "Alle Programme im angegebenen Verzeichnis wurden ausgeführt. Nothing to do here!"
+pause
+return
